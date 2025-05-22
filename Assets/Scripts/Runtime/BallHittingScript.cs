f758176d6fe0da62606d34c8c4ba0fcde3321456
AJ Roberts/Home <ajroberts2751@gmail.com>
2025-05-22 12:02:43 -0700

    Tweaked ball and ground phyisics materials, tweaked ball rigidbody settings, added movement to orbital cam tied to mouse, created and began laying out prototype ball hitting script

---
+++ Assets/Scripts/Runtime/BallHittingScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class BallHittingScript : MonoBehaviour
+{
+    //hide and keep mouse centered to screen at all times
+    //basic drag and release golf shot mechanic
+    //on click, lock camera vertical movement
+    //draw line from ball backwards x distance
+    //on release, hit ball with a a base power multiplied by the amount pulled back
+
+
+}
