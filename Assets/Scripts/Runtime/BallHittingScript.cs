afc8d01fbf2ce2ec0e48e2c65a4af1d197d4ee72
AJ Roberts/Home <ajroberts2751@gmail.com>
2025-05-28 13:12:56 -0700

    Added new shot mode: drag-to-shoot. Uses mouse movement to determine shot power, added first attempt at stopping check opnce ball is rolling slow enough. Tweaked min and max shot force values

--- Assets/Scripts/Runtime/BallHittingScript.cs
+++ Assets/Scripts/Runtime/BallHittingScript.cs
@@ -16,13 +16,22 @@ public class BallHittingScript : MonoBehaviour
     [SerializeField] protected Transform _ballAimTarget;
     [SerializeField] protected Transform _ballAimOrigin;
     [SerializeField] protected CinemachineInputAxisController _cameraAxisController;
+    [SerializeField] protected CinemachineCamera _cinemachineCamera;
+    [SerializeField] protected float _baseFOV = 60f;
+    [SerializeField] protected float _aimingFOV = 40f;
     [SerializeField] protected float _maxShotTime = 2f;
+    [SerializeField] protected Vector3 _restVelocity;
+    [SerializeField] protected float _timeBeforeRest = 1f;
 
     private bool _isAiming = false;
     private bool _isShooting = false;
+    private bool _isBallShot = false;
+    private float _shotPowerNormalized = 0f;
     private float _shotPower = 0f;
+    private float _maxShotPower = 100f;
     private float _shotTime = 0f;
-
+    private float _deltaShotPower;
+    private float _restTimer;
 
     //hide and keep mouse centered to screen at all times
     //basic drag and release golf shot mechanic
@@ -40,24 +49,63 @@ public class BallHittingScript : MonoBehaviour
     private void FixedUpdate()
     {
         print("_isAiming = " + _isAiming);
+        print("_isBallShot = " + _isBallShot);
 
         SetAimTargetTransform();
+
+        //print("RigidBody Linear Velocity: " + _golfBallRB.linearVelocity);
+
+
+        //if the ball is moving slow enough for a period of time, stop it
+        if(_isBallShot &&
+            _golfBallRB.linearVelocity.x <= _restVelocity.x &&
+            _golfBallRB.linearVelocity.y <= _restVelocity.y &&
+            _golfBallRB.linearVelocity.z <= _restVelocity.z)
+        {
+            _restTimer += Time.deltaTime;
+        }
+
+        if(_restTimer >= _timeBeforeRest)
+        {
+            _golfBallRB.linearVelocity = Vector3.zero;
+            _isBallShot = false;
+            _restTimer = 0f;
+        }
 
     }
 
     private void Update()
     {
-        if (_isShooting)
-        {
-            _shotTime += Time.deltaTime;
 
-            _shotPower = Mathf.InverseLerp(0f, _maxShotTime, _shotTime);
 
-            _shotPowerBarFillImage.fillAmount = _shotPower;
-        }
+
+        //for hold-to-charge shooting mode
+        //if (_isShooting)
+        //{
+        //    _shotTime += Time.deltaTime;
+
+        //    _shotPower = Mathf.InverseLerp(0f, _maxShotTime, _shotTime);
+
+        //    _shotPowerBarFillImage.fillAmount = _shotPowerNormalized;
+        //}
 
     }
 
+    public void OnLook(InputValue value)
+    {
+        
+
+        //for click-and-drag shooting mode
+
+        if (!_isShooting) return;
+
+        _deltaShotPower = value.Get<Vector2>().y;
+
+        _shotPower += _deltaShotPower;
+
+        _shotPowerNormalized = Mathf.InverseLerp(0f, _maxShotPower, _shotPower);
+        _shotPowerBarFillImage.fillAmount = _shotPowerNormalized;
+    }
 
     public void OnStartAiming(InputValue value)
     {
@@ -66,6 +114,8 @@ public class BallHittingScript : MonoBehaviour
         //show power bar
         _shotPowerBarCanvas.enabled = true;
 
+
+        _cinemachineCamera.Lens.FieldOfView = _aimingFOV;
     }
 
     public void OnStopAiming(InputValue value)
@@ -74,6 +124,9 @@ public class BallHittingScript : MonoBehaviour
         _isShooting = false;
         //hide power bar
         _shotPowerBarCanvas.enabled = false;
+
+
+        _cinemachineCamera.Lens.FieldOfView = _baseFOV;
     }
 
     public void OnStartShooting(InputValue value)
@@ -93,18 +146,22 @@ public class BallHittingScript : MonoBehaviour
         
         
         //add force to ball based on shot charge time
-        float _shotForce = Mathf.Lerp(_minShotForce, _maxShotForce, _shotPower);
-        Vector3 _direction = (_ballAimOrigin.position - _ballAimTarget.position).normalized;
+        float _shotForce = Mathf.Lerp(_minShotForce, _maxShotForce, _shotPowerNormalized);
+        Vector3 _direction = (_ballAimTarget.position - _ballAimOrigin.position).normalized;
 
         print("Shot Force: " + _shotForce);
         
 
         _golfBallRB.AddForce(_direction * _shotForce,ForceMode.Impulse);
 
+        _isBallShot = true;
 
         _isShooting = false;
         _cameraAxisController.enabled = true;
 
+        _shotPower = 0f;
+        _shotPowerNormalized = 0f;
+        _shotPowerBarFillImage.fillAmount = 0f;
     }
 
     private void OnDrawGizmos()
